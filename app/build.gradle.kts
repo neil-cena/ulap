dc090ee26bccf8a3d13126892201576ee52079ee
Neil Allen Cena <neilcena.dev@gmail.com>
2026-04-14 00:21:05 +0800

    chore: add .gitattributes for consistent line endings and update gradlew script; bump versionName to 1.0.5 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
