2507ea7f3259291ef85cde36ca06d138ea9a62d7
Neil Allen Cena <neilcena.dev@gmail.com>
2026-04-18 04:24:17 +0800

    chore: bump version to 1.0.13 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.ulap"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.12"
+        versionCode = 8
+        versionName = "1.0.13"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
